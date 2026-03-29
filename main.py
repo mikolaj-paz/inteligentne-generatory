d11b14c753caf7a5ac1f587c8e808d2f844c30b0
Mikolaj <mikolajpaz@gmail.com>
2026-03-29 22:20:13 +0200

    Run formatter

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from seeder.core.schema import fetch_schema
 from seeder.core.engine import run
 from seeder.models import Config, Schema
 
+
 @click.command()
 @click.option(
     "--config",
@@ -35,7 +36,7 @@ def main(config_path, dry_run) -> None:
         _print_dry_run(requests)
         connection.close()
         return
-    
+
     # TODO
     # try:
     #     schema = fetch_schema(connection)
@@ -46,6 +47,7 @@ def main(config_path, dry_run) -> None:
     #     click.echo(f"Error: {e}", err=True)
     # finally:
     #     connection.close()
+
 
 def _validate_requests(requests: Config, schema: Schema) -> None:
     """Check that all requested tables actually exist in the database."""
@@ -59,7 +61,6 @@ def _print_dry_run(requests: Config) -> None:
         for column_name, column_config in request.column_overrides.items():
             if column_config.generator:
                 click.echo(f"\t\t{column_name}: generator='{column_config.generator}'")
-            
 
 
 if __name__ == "__main__":
