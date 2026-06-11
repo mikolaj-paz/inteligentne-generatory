f2b3dfbc919de82f7a45019a84d395d2816c5082
Dorota <dm.ptasznik@gmail.com>
2026-06-11 20:45:22 +0200

    feat: add balance generator

---
+++ seeder/generation/types/balance.py
@@ -0,0 +1,9 @@
+import random
+from seeder.generation.base import BaseGenerator
+
+class BalanceGenerator(BaseGenerator):
+    name = "saldo"
+
+    def generate(self, context: dict = None, **kwargs) -> float:
+        balance = random.uniform(-100.0, 50000.0)
+        return round(balance, 2)
